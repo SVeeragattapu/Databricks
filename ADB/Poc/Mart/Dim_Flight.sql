2142d31a107d26d23a2229c9c82e752b55d45860
SVeeragattapu <149193251+SVeeragattapu@users.noreply.github.com>
2023-10-31 21:23:27 +0530

    Update: ADB README.md

--- ADB/Poc/Mart/Dim_Flight.sql
+++ ADB/Poc/Mart/Dim_Flight.sql
@@ -8,10 +8,6 @@ DESC cleansed_poc.flight
 
 -- COMMAND ----------
 
-DROP TABLE Reporting_Flight
-
--- COMMAND ----------
-
 CREATE TABLE IF NOT EXISTS Reporting_Flight (
   date DATE,
   ArrDelay INT,
@@ -23,20 +19,35 @@ CREATE TABLE IF NOT EXISTS Reporting_Flight (
   FlightNum INT,
   TailNum STRING,
   deptime STRING
-) USING DELTA LOCATION '/mnt/mart_datalake/Reporting_Flight'
+) USING DELTA 
+PARTITIONED BY (date_year int)
+LOCATION '/mnt/mart_datalake/Reporting_Flight'
 
 -- COMMAND ----------
 
-INSERT OVERWRITE Reporting_Flight
-SELECT
-  date,
-  ArrDelay,
-  DepDelay,
-  Origin,
-  Cancelled,
-  CancellationCode,
-  UniqueCarrier,
-  FlightNum,
-  TailNum,
-  deptime
-  FROM cleansed_poc.flight
+-- MAGIC %py
+-- MAGIC #dbutils.fs.rm('/mnt/mart_datalake/Reporting_Flight',True)
+
+-- COMMAND ----------
+
+-- MAGIC %py
+-- MAGIC max_year = spark.sql("SELECT year(max(date)) from cleansed_poc.flight").collect()[0][0]
+-- MAGIC #max_year=2007
+
+-- COMMAND ----------
+
+-- MAGIC %py
+-- MAGIC spark.sql(f"""
+-- MAGIC INSERT OVERWRITE Reporting_Flight PARTITION (date_year = {max_year})
+-- MAGIC SELECT
+-- MAGIC   date,
+-- MAGIC   ArrDelay,
+-- MAGIC   DepDelay,
+-- MAGIC   Origin,
+-- MAGIC   Cancelled,
+-- MAGIC   CancellationCode,
+-- MAGIC   UniqueCarrier,
+-- MAGIC   FlightNum,
+-- MAGIC   TailNum,
+-- MAGIC   deptime
+-- MAGIC   FROM cleansed_poc.flight where year(date)={max_year} """)
