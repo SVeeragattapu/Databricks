5892ac0c9dac2819637f75bac9ae781d75dd58de
SVeeragattapu <149193251+SVeeragattapu@users.noreply.github.com>
2023-10-31 21:22:58 +0530

    commit1

---
+++ ADB/Poc/Mart/Dim_Plane.sql
@@ -0,0 +1,44 @@
+-- Databricks notebook source
+-- MAGIC %sql
+-- MAGIC use mart_poc;
+
+-- COMMAND ----------
+
+DESC cleansed_poc.plane
+
+-- COMMAND ----------
+
+DROP TABLE DIM_PLANE
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS DIM_PLANE (
+  tailid STRING,
+  type STRING,
+  manufacturer STRING,
+  issue_date DATE,
+  model STRING,
+  status STRING,
+  aircraft_type STRING,
+  engine_type STRING,
+  year INT
+) USING DELTA LOCATION '/mnt/mart_datalake/DIM_PLANE'
+
+-- COMMAND ----------
+
+-- MAGIC %fs mounts
+
+-- COMMAND ----------
+
+INSERT OVERWRITE DIM_PLANE
+SELECT
+ tailid,
+  type,
+  manufacturer,
+  issue_date,
+  model,
+  status,
+  aircraft_type,
+  engine_type,
+  year
+  FROM cleansed_poc.plane
